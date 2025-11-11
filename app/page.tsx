ba0e72d921b664f96ff0ea254f2b2284e321bc89
mike <mike.melchior@dcmail.ca>
2025-11-11 12:38:51 -0500

    update button text to reflect jpg dl

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function HomePage() {
         if (!qrData) return;
         const a = document.createElement("a");
         a.href = qrData;
-        a.download = `QR_CODE_${title}.png`;
+        a.download = `QR_CODE_${title}.jpg`;
         a.click();
     }
 
@@ -50,7 +50,7 @@ export default function HomePage() {
                         onClick={handleDownload}
                         className="mt-4 bg-green-600 text-white px-3 py-1 rounded hover:bg-green-700"
                     >
-                        Download PNG
+                        Download JPG
                     </button>
                 </div>
             )}
